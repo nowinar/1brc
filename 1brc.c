7e36c2d1c44ead31b7bbcaed4e410dad4b41ab2e
nowwincz <tprochazka2@post.cz>
2024-05-01 18:40:33 +0200

    fairly optimized version

--- 1brc.c
+++ 1brc.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
+#include <stdbool.h>
 typedef struct {
     char city[100];
     float temperature;
@@ -36,6 +37,12 @@ float* getMinMaxAvgFromArray(float a[1000], int count){
 }
 
 int main(){
+    //time measuring
+    struct timespec start, end;
+    long long elapsed_time; // čas v milisekundách
+
+    // Zaznamenává startovní čas
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
     FILE *file;
     char *filename = "./measurements.txt";
@@ -68,25 +75,49 @@ int main(){
         Array = realloc(Array, (count + 1) * sizeof(CityTemperature));
     }
     fclose(file);
+    char **cities = malloc(1 * sizeof(char *));
+    int citiesCount = 0;
     //printf("%d", count);
     for(int i = 0; i<= count; i++){
         char city[100];
         strncpy(city, Array[i].city, sizeof(city));
+        //till here cities are allright
+        //printf("%s start\n", Array[i].city);
         int numsCount = 0;
         float nums[1000];
         int d;
-        for(d = 0; d<=count; d++){
-            //printf("%s\n",city);
-
-            if(strcmp(Array[d].city, city) == 0){
-                //printf("%f\n", Array[d].temperature);
-                nums[numsCount] = Array[d].temperature;
-                numsCount++;
+        //printf("%s\n", city);
+        bool ShouldFind = false;
+        for(int j = 0; j<citiesCount; j++){
+            if(strcmp(city, cities[j]) == 0){
+                //printf("%s", city);
+                ShouldFind = true;
             }
         }
-        float* vys = getMinMaxAvgFromArray(nums, numsCount);
+        if(!ShouldFind){
+            //printf("%s start\n%s\n", Array[i].city, city);
+            for(d = 0; d<=count; d++){
+                //printf("%s\n",city);
+                if(strcmp(Array[d].city, city) == 0){
+                    //printf("%f\n", Array[d].temperature);
+                    nums[numsCount] = Array[d].temperature;
+                    numsCount++;
+                }
+            }
+            float* vys = getMinMaxAvgFromArray(nums, numsCount);
 
-        printf("Mesto: %s, prumer: %f, min: %f, max:%f\n", city, vys[2],vys[0],vys[1]);
+            printf("Mesto: %s, prumer: %f, min: %f, max:%f\n", city, vys[2],vys[0],vys[1]);
+            cities = realloc(cities, (citiesCount+1) * sizeof(char *));
+            cities[citiesCount] = strdup(city);
+            citiesCount++;
+            printf("%d",citiesCount);
+            }
+        
     }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    // Výpočet uplynulého času v milisekundách
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("\n%lldms\n", elapsed_time);
     return 0;
 }
